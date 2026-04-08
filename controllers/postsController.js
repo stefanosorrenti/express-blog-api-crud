4c53140f908b9c0ef9fb559932e9e7fe9f1ef064
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-04-08 11:59:01 +0200

    MILESTONE 2: add data file and logic for post controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,31 +1,38 @@
 
+const macchine = require('../data/macchine')
 //INDEX CONTROLLER
 const index = (req, res) => {
     console.log('Sei nella rotta INDEX')
 
-    res.send('Queste sono tutte le mie risorse')
+    res.json(macchine)
 }
 
 //STORE CONTROLLER
 const store = (req, res) => {
-  console.log('Sei nella rotta STORE');
+    console.log('Sei nella rotta STORE');
 
-  res.send('Stai aggiungendo una nuova risorsa.')
+    res.send('Stai aggiungendo una nuova risorsa.')
 }
 
 //SHOW CONTROLLER
 const show = (req, res) => {
-    const id = req.params.id
+    const id = parseInt(req.params.id)
     console.log('Sei nella rotta SHOW')
+    const macchina = macchine.find(macchina => macchina.id === id)
+    if (!macchina) {
 
-    res.send(`Stai visualizzando  la risorsa numero ${id}`)
+        res.status(404)
+        console.log('ERRORE: Macchina non trovata');
+        return res.send('Macchina non trovata!')
+    }
 
+    res.send(macchina)
 }
 
 //UPDATE CONTROLLER
 const update = (req, res) => {
     console.log('Sei nella rotta UPDATE')
-    const id = req.params.id
+    const id = parseInt(req.params.id)
 
     res.send(`Sta aggiornado la risorsa numero ${id}`)
 
@@ -34,7 +41,7 @@ const update = (req, res) => {
 //MODIFY CONTROLLER
 const modify = (req, res) => {
     console.log('Sei nella rotta MODIFY')
-    const id = req.params.id
+    const id = parseInt(req.params.id)
 
     res.send(`Sta aggiorando UNA PARTE della risorsa ${id}`)
 
@@ -45,9 +52,18 @@ const modify = (req, res) => {
 
 const destroy = (req, res) => {
     console.log('Sei nella rotta DESTROY')
-    const id = req.params.id
+    const id = parseInt(req.params.id)
+    const macchina = macchine.find(macchina => macchina.id === id)
+    if (!macchina) {
+        res.status(404)
+        res.send('Elemento non trovato')
+    } else {
+        macchine.splice(macchine.indexOf(macchina), 1)
+        console.log(macchine);
+        res.sendStatus(204)
+        
 
-    res.send(`Sto eliminando la risorsa ${id}`)
+    }
 
 }
 
